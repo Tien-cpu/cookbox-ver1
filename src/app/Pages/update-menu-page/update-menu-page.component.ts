28a8a08e898dc009765b5999a5117a6ed22b7e44
ncc304 <nguyenchicuong304@gmail.com>
2021-11-05 01:03:05 +0700

    05112021

--- src/app/Pages/update-menu-page/update-menu-page.component.ts
+++ src/app/Pages/update-menu-page/update-menu-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { forEach } from 'jszip';
 import { Menu } from 'src/app/Models/Menu';
 import { MenuStore } from 'src/app/Models/MenuStore';
 import { Sessions } from 'src/app/Models/Sessions';
@@ -24,7 +25,8 @@ export class UpdateMenuPageComponent implements OnInit {
 
   public listMenu: Menu[] = [];
   public listSession: Sessions[] = [];
-
+  // public selectedMenu:any;
+  public selectedSession:any;
   listStatus: {
     "value" : string,
     "key": string,
@@ -67,12 +69,16 @@ export class UpdateMenuPageComponent implements OnInit {
       }else{
         this.selectstatus = this.listStatus[0].key;
       }
-
+      // this.selectedMenu = res.menu_id;
+      this.selectedSession = res.session_id;
 
     });
 
   }
-
+  changeStatus(event : any){
+    this.menuID = event.target.value;
+    console.log(this.menuID); // log menu ID
+  }
   changeMenu(event : any){
     this.menuID = event.target.value;
     console.log(this.menuID); // log menu ID
@@ -82,7 +88,30 @@ export class UpdateMenuPageComponent implements OnInit {
     console.log(this.sessionID); // log session ID
   }
   updateMenu(){
-
+    let menuStore:{
+      "id": number,
+      "menu_id": number,
+      "store_id": number,
+      "store_name": string,
+      "session_id": number,
+      "status": boolean
+    } = {
+      id : this.menuStore.id,
+      menu_id : this.menuStore.menu_id,
+      store_id : this.menuStore.store_id,
+      store_name : this.menuStore.store_name,
+      session_id : this.sessionID,
+      status : true
+    };
+    if(this.selectstatus === 'open'){
+      menuStore.status = true;
+    }else{
+      menuStore.status = false;
+    }
+    console.log('menuStore', menuStore);
+    this.menuStoreService.updateMenuInStore(menuStore).subscribe(res=>{
+      this.router.navigate(['menu-main-page']);
+    })
   }
   goHomePage() {
     this.router.navigate(['home']);
